d1e7455a9ebf8ae6c9c201922b46e941292e586b
Shikha <sbhatt10@uncc.edu>
2019-02-18 20:01:12 -0500

    First GET connection

--- app/src/main/java/com/example/inclass05/MainActivity.java
+++ app/src/main/java/com/example/inclass05/MainActivity.java
@@ -3,10 +3,18 @@ package com.example.inclass05;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +26,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (isConnected()){
-                    Toast.makeText(MainActivity.this, "Internet Connection", Toast.LENGTH_SHORT).show();
+                    new GetDataAsync().execute("http://dev.theappsdr.com/apis/photos/keywords.php");
                 }
                 else{
-                    Toast.makeText(MainActivity.this, "Is Not Connected", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Internet is Not Connected", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -35,5 +43,44 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+    private class GetDataAsync extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder stringBuilder = new StringBuilder();
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            String result = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    result = stringBuilder.toString();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
